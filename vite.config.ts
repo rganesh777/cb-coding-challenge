7461ba29a81e8027fb32f1133497e81239fffec1
Ganesh Rajendran <rganesh777@gmail.com>
2025-12-11 18:30:07 +1100

    Deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/cb-coding-challenge-requirements-frontend/cb%20coding%20challenge/",
   plugins: [
     react({
       babel: {
